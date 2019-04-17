bf1e2c0edb4a9394c619dd93100f117ae3bdc7c2
Trayonna <trayonna.hendricks@journalism.cuny.edu>
2019-04-16 23:32:40 -0400

    feedback edits

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 $("body").on("change", ".filter", function (e) {
 	e.preventDefault();
 
+	/* This console log is to make sure the filter works altogether making sure each page appears with its own picture and list */
 	console.log("Hey, Y'all!");
 
 	const $this_filter = $(this);
 	const borough_type = $this_filter.val();
-
+	/* This console log is to make sure each borough selection in the filter works*/
 	console.log("Borough type " + borough_type);
-
+	/* This is code is for the button for the filter */
 	$(".item.active").removeClass("active");
 	$("." + borough_type).addClass("active");
 
